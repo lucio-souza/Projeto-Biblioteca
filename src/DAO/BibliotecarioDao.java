077b5d15f211806de892353deafe8e520d9f0c1f
lucio-souza <desouzaleitelucioweslley@gmail.com>
2023-12-05 03:03:03 -0300

    criando a classe BibliotecarioDao e os metodos do CRUD

--- src/DAO/BibliotecarioDao.java
+++ src/DAO/BibliotecarioDao.java
@@ -1,22 +1,22 @@
 package DAO;
 
+import model.Bibliotecario;
+import java.util.List;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.ArrayList;
-import model.Cliente;
 
-public class ClienteDao {
+public class BibliotecarioDao {
 	
 	@SuppressWarnings("finally")
-	public List<Cliente> getAll(){
-		String sql="select * from Cliente";
-		List<Cliente> clientes=new ArrayList<>();
+	public List<Bibliotecario> getAll(){
+		String sql="select * from Bibliotecario";
 		Connection conn=null;
 		PreparedStatement pstm=null;
 		ResultSet rset=null;
+		List<Bibliotecario> bibliotecarios=new ArrayList<>();
 		
 		try {
 			conn=ConexaoDAO.conectarBD();
@@ -24,70 +24,46 @@ public class ClienteDao {
 			rset=pstm.executeQuery();
 			
 			while(rset.next()) {
-				Cliente cliente=new Cliente();
-				cliente.setId(rset.getInt("Id"));
-				cliente.setCpf(rset.getString("cpf"));
-				cliente.setNome(rset.getString("nome"));
-				cliente.setTelefone(rset.getString("telefone"));
-				clientes.add(cliente);
+				Bibliotecario bibliotecario=new Bibliotecario();
+				bibliotecario.setId(rset.getInt("id"));
+				bibliotecario.setEmail(rset.getString("email"));
+				bibliotecario.setSenha(rset.getInt("senha"));
+				bibliotecarios.add(bibliotecario);
 			}
-			conn.close();
-			pstm.close();
-			rset.close();
 		}catch(SQLException ex) {
 			ex.printStackTrace();
-		}finally{
-			return clientes;
+		}finally {
+			return bibliotecarios;
 		}
 	}
 	
-	public Cliente getOneById(int id) {
-		String sql="select * from Cliente where id=?";
+	public Bibliotecario getOneById(int id) {
+		String sql="select * from Bibliotecario where id=?";
 		Connection conn=null;
 		PreparedStatement pstm=null;
 		ResultSet rset=null;
 
 		try {		
-			Cliente cliente=new Cliente();
+			Bibliotecario bibliotecario=new Bibliotecario();
 			conn=ConexaoDAO.conectarBD();
 			pstm=conn.prepareStatement(sql);
 			pstm.setInt(1, id);
 			rset=pstm.executeQuery();
 
 			if(rset.next()) {
-				cliente.setId(rset.getInt("Id"));
-				cliente.setCpf(rset.getString("cpf"));
-				cliente.setNome(rset.getString("nome"));
-				cliente.setTelefone(rset.getString("telefone"));
+				bibliotecario.setId(rset.getInt("Id"));
+				bibliotecario.setEmail(rset.getString("email"));
+				bibliotecario.setSenha(rset.getInt("senha"));
 			}
-			return cliente;
+			return bibliotecario;
 		}catch(SQLException ex) {
 			ex.printStackTrace();
 		}
 		return null;
 	}
 	
-	public void create(Cliente cliente) {
-		String sql="insert into Cliente(cpf,nome,telefone) values(?,?,?)";
-		Connection conn=null;
-		PreparedStatement pstm=null;
-		
-		try {
-			conn=ConexaoDAO.conectarBD();
-			pstm=conn.prepareStatement(sql);
-			pstm.setString(1, cliente.getCpf());
-			pstm.setString(2, cliente.getNome());
-			pstm.setString(3, cliente.getTelefone());
-			pstm.execute();
-			conn.close();
-			pstm.close();
-		}catch(SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
 	public void delete(int id) {
-		String sql="delete from Cliente where id = ?";
+		String sql="delete from Bibliotecario where id=?";
 		Connection conn=null;
 		PreparedStatement pstm=null;
 		
@@ -103,18 +79,35 @@ public class ClienteDao {
 		}
 	}
 	
-	public void update(Cliente cliente) {
-		String sql="update Cliente set cpf=?,nome=?,telefone=? where id=?";
+	public void create(Bibliotecario bibliotecario){
+		String sql="insert into Cliente(email,senha) values(?,?)";
 		Connection conn=null;
 		PreparedStatement pstm=null;
 		
 		try {
 			conn=ConexaoDAO.conectarBD();
 			pstm=conn.prepareStatement(sql);
-			pstm.setString(1, cliente.getCpf());
-			pstm.setString(2, cliente.getNome());
-			pstm.setString(3, cliente.getTelefone());
-			pstm.setInt(4, cliente.getId());
+			pstm.setString(1, bibliotecario.getEmail());
+			pstm.setInt(2, bibliotecario.getSenha());
+			pstm.execute();
+			conn.close();
+			pstm.close();
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void update(Bibliotecario bibliotecario) {
+		String sql="update Livro set email=?,senha=? where id=?";
+		Connection conn=null;
+		PreparedStatement pstm=null;
+		
+		try {
+			conn=ConexaoDAO.conectarBD();
+			pstm=conn.prepareStatement(sql);
+			pstm.setString(1,bibliotecario.getEmail());
+			pstm.setInt(2, bibliotecario.getSenha());
+			pstm.setInt(3, bibliotecario.getId());
 			pstm.executeUpdate();
 			conn.close();
 			pstm.close();
